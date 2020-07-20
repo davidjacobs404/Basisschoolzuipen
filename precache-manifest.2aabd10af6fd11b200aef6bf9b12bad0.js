d3bc8bf44ef47edaa33cd495cc7c6e412380e71d
davidjacobs404 <david.bas@hotmail.nl>
2020-07-20 17:47:49 +0200

    Updates

---
+++ precache-manifest.2aabd10af6fd11b200aef6bf9b12bad0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4b4ae6e6619a94305ac34433967cc97",
+    "url": "/basisschoolzuipen/index.html"
+  },
+  {
+    "revision": "81e3aee9182cda94d789",
+    "url": "/basisschoolzuipen/static/css/main.8be2298f.chunk.css"
+  },
+  {
+    "revision": "ad908fff3c4b1f7dc6b0",
+    "url": "/basisschoolzuipen/static/js/2.b9bd2790.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/basisschoolzuipen/static/js/2.b9bd2790.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81e3aee9182cda94d789",
+    "url": "/basisschoolzuipen/static/js/main.68289114.chunk.js"
+  },
+  {
+    "revision": "e066ff5896c469af8bf1",
+    "url": "/basisschoolzuipen/static/js/runtime-main.6379ffb9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/basisschoolzuipen/static/media/logo.5d5d9eef.svg"
+  }
+]);
